bff2f9fb5d40847a26015161f82b24e37a93432c
CSE340 Student <ryan.steven.patterson@gmail.com>
2017-11-01 16:41:53 -0700

    files exist in repo.

---
+++ parser.cc
@@ -0,0 +1,266 @@
+/*
+ * Copyright (C) Rida Bazzi, 2017
+ *
+ * Do not share this file with anyone
+ *
+ * Do not post this file or derivatives of 
+ * of this file online
+ *
+ */
+#include <iostream>
+#include <cstdlib>
+#include "parser.h"
+
+using namespace std;
+
+void Parser::syntax_error()
+{
+    cout << "Syntax Error\n";
+    exit(1);
+}
+
+// this function gets a token and checks if it is
+// of the expected type. If it is, the token is
+// returned, otherwise, synatx_error() is generated
+// this function is particularly useful to match
+// terminals in a right hand side of a rule.
+// Written by Mohsen Zohrevandi
+Token Parser::expect(TokenType expected_type)
+{
+    Token t = lexer.GetToken();
+    if (t.token_type != expected_type)
+        syntax_error();
+    return t;
+}
+
+// this function simply checks the next token without
+// consuming the input
+// Written by Mohsen Zohrevandi
+Token Parser::peek()
+{
+    Token t = lexer.GetToken();
+    lexer.UngetToken(t);
+    return t;
+}
+
+// Parsing
+
+void Parser::parse_program()
+{
+    // program -> scope
+    
+    parse_scope();
+}
+
+void Parser::parse_scope()
+{
+    // scope -> { scope_list }
+    
+    expect(LBRACE);
+    parse_scope_list();
+    expect(RBRACE);
+}
+
+
+void Parser::parse_scope_list()
+{
+    // scope_list -> stmt
+    // scope_list -> scope
+    // scope_list -> declaration
+    // scope_list -> stmt scope_list
+    // scope_list -> scope scope_list
+    // scope_list -> declaration scope_list
+    
+    //TODO: needs testing
+    Token t = peek();
+    if(t.token_type == stmt) {
+	parse_stmt();
+	t = peek();
+	if(t.token_type == scope_list)
+	    parse_scope_list();
+    }
+    if(t.token_type == scope) {
+	parse_scope();
+ 	t = peek();
+	if(t.token_type == scope_list)
+	    parse_scope_list();
+    }
+    if(t.token_type == declaration) {
+	parse_declaration();
+	t = peek();
+	if(t.token_type == scope_list)
+	    parse_scope_list();
+    }
+}
+
+void Parser::parse_declaration()
+{
+    // declaration -> type_decl
+    // declaration -> var_decl
+
+    Token t = peek();
+    if (t.token_type == TYPE)
+    	parse_type_decl();
+    if (t.token_type == VAR)
+    	parse_var_decl();
+}
+
+void Parser::parse_type_decl()
+{
+    // type_decl -> TYPE id_list COLON type_name SEMICOLON
+
+    expect(TYPE);
+    parse_id_list();
+    expect(COLON);
+    parse_type_name();
+    expect(SEMICOLON);
+}
+
+void Parser::parse_type_name()
+{
+    // type_name -> REAL
+    // type_name -> INT
+    // type_name -> BOOLEAN
+    // type_name -> STRING
+    // type_name -> LONG
+    // type_name -> ID
+    
+
+
+
+
+    // TODO
+}
+
+void Parser::parse_var_decl()
+{
+    // var_decl -> VAR id_list COLON type_name SEMICOLON
+
+    expect(VAR);
+    parse_id_list();
+    expect(COLON);
+    parse_type_name();
+    expect(SEMICOLON);
+}
+
+void Parser::parse_id_list()
+{
+    // id_list -> ID
+    // id_list -> ID COMMA id_list
+
+    // TODO
+}
+
+
+
+void Parser::parse_stmt_list()
+{
+    // stmt_list -> stmt
+    // stmt_list -> stmt stmt_list
+    
+    parse_stmt();
+    Token t = peek();
+    if (t.token_type == WHILE || t.token_type == ID)
+    {
+        // stmt_list -> stmt stmt_list
+        parse_stmt_list();
+    }
+    else if (t.token_type == RBRACE)
+    {
+        // stmt_list -> stmt
+    }
+    else
+    {
+        syntax_error();
+    }
+}
+
+void Parser::parse_stmt()
+{
+    // stmt -> assign_stmt
+    // stmt -> while_stmt
+ 
+    // TODO
+    
+}
+
+void Parser::parse_assign_stmt()
+{
+    // assign_stmt -> ID EQUAL expr SEMICOLON
+
+    // TODO
+}
+
+void Parser::parse_while_stmt()
+{
+   // while_stmt -> WHILE condition LBRACE stmt list RBRACE
+
+    // TODO
+}
+
+void Parser::parse_expr()
+{
+    // expr -> term 
+    // expr -> term + expr
+
+    // TODO
+}
+
+void Parser::parse_term()
+{
+    // term -> factor
+    // term -> factor MULT term
+
+    // TODO
+}
+
+void Parser::parse_factor()
+{
+    // factor -> LPAREN expr RPAREN
+    // factor -> NUM
+    // factor -> REALNUM
+    // factor -> ID
+
+    // TODO
+}
+
+void Parser::parse_condition()
+{
+    // condition -> ID
+    // condition -> primary relop primary
+
+    // TODO
+}
+
+void Parser::parse_primary()
+{
+    // primary -> ID
+    // primary -> NUM
+    // primary -> REALNUM
+    // TODO
+}
+
+void Parser::parse_relop()
+{
+    // relop -> GREATER
+    // relop -> GTEQ
+    // relop -> LESS
+    // relop -> NOTEQ
+    // relop -> LTEQ
+
+    // TODO
+}
+
+void Parser::ParseInput()
+{
+    parse_program();
+    expect(END_OF_FILE);
+}
+
+int main()
+{
+    Parser parser;
+
+    parser.ParseInput();
+}
+
